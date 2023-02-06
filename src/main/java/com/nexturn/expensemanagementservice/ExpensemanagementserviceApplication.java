13e8743a0568e4266b8e40e146073a6f89882184
manis <manishkumartiwari1308@gmail.com>
2023-02-06 11:19:49 +0530

    Expense Management Service main file commit.

---
+++ src/main/java/com/nexturn/expensemanagementservice/ExpensemanagementserviceApplication.java
@@ -0,0 +1,13 @@
+package com.nexturn.expensemanagementservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpensemanagementserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExpensemanagementserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nexturn/expensemanagementservice/ExpensemanagementserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nexturn.expensemanagementservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpensemanagementserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
